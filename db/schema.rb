7bba2af8acc511457e01f6444275ac37af2a99ef
Camnh <hongcam941997@gmail.com>
2018-05-29 16:33:36 +0700

    data

--- db/schema.rb
+++ db/schema.rb
@@ -12,26 +12,26 @@
 
 ActiveRecord::Schema.define(version: 20180517085100) do
 
-  create_table "answers", force: :cascade do |t|
+  create_table "answers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
     t.boolean "correct_status", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "question_id"
+    t.bigint "question_id"
     t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
-  create_table "categories_courses", force: :cascade do |t|
+  create_table "categories_courses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.boolean "status", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
-    t.integer "course_id"
+    t.bigint "user_id"
+    t.bigint "course_id"
     t.index ["course_id"], name: "index_categories_courses_on_course_id"
     t.index ["user_id"], name: "index_categories_courses_on_user_id"
   end
 
-  create_table "courses", force: :cascade do |t|
+  create_table "courses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
     t.datetime "dateDay"
     t.boolean "is_delete", default: false
@@ -40,26 +40,26 @@ ActiveRecord::Schema.define(version: 20180517085100) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "questions", force: :cascade do |t|
+  create_table "questions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
     t.boolean "is_delete", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "wordlist_id"
+    t.bigint "wordlist_id"
     t.index ["wordlist_id"], name: "index_questions_on_wordlist_id"
   end
 
-  create_table "results", force: :cascade do |t|
+  create_table "results", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "question_id"
     t.integer "answer_id"
     t.boolean "choose_user", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_results_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", default: ""
     t.string "encrypted_password", default: ""
     t.boolean "role", default: false
@@ -68,7 +68,7 @@ ActiveRecord::Schema.define(version: 20180517085100) do
     t.string "address", default: ""
     t.string "name", default: ""
     t.boolean "is_delete", default: false
-    t.datetime "dateOfbirth"
+    t.datetime "dateOfbirth", default: -> { "CURRENT_TIMESTAMP" }
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -85,14 +85,20 @@ ActiveRecord::Schema.define(version: 20180517085100) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table "wordlists", force: :cascade do |t|
+  create_table "wordlists", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
     t.string "from_translator"
     t.boolean "is_delete", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "course_id"
+    t.bigint "course_id"
     t.index ["course_id"], name: "index_wordlists_on_course_id"
   end
 
+  add_foreign_key "answers", "questions"
+  add_foreign_key "categories_courses", "courses"
+  add_foreign_key "categories_courses", "users"
+  add_foreign_key "questions", "wordlists"
+  add_foreign_key "results", "users"
+  add_foreign_key "wordlists", "courses"
 end
